7b3c3399a51111bbcded4fcf0dc22306cd75fa19
Umarbek Yoqubov <umarbekyoqubov39@gmail.com>
2026-05-02 14:23:30 +0300

    vazifa

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// 1. user nomli object yarating. Ichida: name va age bo‘lsin. Keyin faqat name ni console.log qiling.
+//  const user={
+//     name:"Umarbek",
+//     age:17,
+//  };
+//  console.log(user.name);
+ 
+
+// 2. product object yarating. Ichida: title va price bo‘lsin. Keyin price ni chiqaring.
+// let product={
+//     title:"gul karom",
+//     price:19000.
+
+// };
+// console.log(product.priceprice);
+
+// 3. student object yarating. Ichida: name va active (true yoki false) bo‘lsin. Keyin active ni chiqaring.
+
+// 4. car object yarating. Ichida: brand, color, year bo‘lsin. Keyin color ni chiqaring.
+// 5. phone object yarating. Ichida: brand va model bo‘lsin. Keyin model ni ['model'] orqali chiqaring.
+// 6. user object yarating. Ichida: name va address object bo‘lsin. address ichida: city bo‘lsin. Keyin
+// city ni chiqaring.
+// 7. users nomli array yarating. Ichida 2 ta object bo‘lsin (name va age bilan). Keyin birinchi
+// objectning name ni chiqaring.
+// 8. student object yarating. Ichida: name va subjects (array) bo‘lsin. subjects ichida 2 ta fan yozing.
+// Keyin birinchi fanni chiqaring.
+// 9. company object yarating. Ichida: name va employees (array) bo‘lsin. employees ichida 2 ta
+// object (name va role) bo‘lsin. Keyin ikkinchi employee role ni chiqaring.
+// 10. user object yarating. Ichida: name, hobbies (array), address (object) bo‘lsin. address ichida city
+// yozing. Keyin hobbies[0] va city ni chiqaring
